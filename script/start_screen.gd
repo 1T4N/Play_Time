632b44f395a50e28c8737e2faddccaf005564dcc
KID <sandagakristian90@gmail.com>
2025-06-06 23:08:40 +0800

    First Commit

---
+++ script/start_screen.gd
@@ -0,0 +1,5 @@
+extends Control
+@onready var touch_the_screen: Button = $"Panel/touch the screen"
+
+func _on_touch_the_screen_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Screen.tscn")
