055c914c831bf648ab9561441459018454b97c72
fylart <fylartthrowaway0@gmail.com>
2025-06-13 11:30:45 +0800

    - UX feature on time deduction - fix hover UX issue on mainscreen (Game catalogue)

--- Game/LetterTracing/TracingData.gd
+++ Game/LetterTracing/TracingData.gd
@@ -2,3 +2,4 @@ extends Node
 var score:int = 0
 
 signal retry
+var timeDeduction:int = 5
--- components/script/animationScalePopUp.gd
+++ components/script/animationScalePopUp.gd
@@ -1,4 +1,6 @@
 extends Button 
+var isFinishedInitializing:bool = false
+
 func _ready() -> void:
 	mouse_entered.connect(_on_button_mouse_entered)
 	mouse_exited.connect(_on_button_mouse_exited)
@@ -16,12 +18,16 @@ func scaleUpAnimation():
 @export var hoverAnimationScale:float = 1.1
 
 func _on_button_mouse_entered():
+	if not isFinishedInitializing:
+		return
 	set_default_cursor_shape(CURSOR_POINTING_HAND)
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self,"scale",Vector2(hoverAnimationScale, hoverAnimationScale), 0.3)
 	pass
 
 func _on_button_mouse_exited():
+	if not isFinishedInitializing:
+		return
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self,"scale",Vector2.ONE, 0.3)
 	pass
