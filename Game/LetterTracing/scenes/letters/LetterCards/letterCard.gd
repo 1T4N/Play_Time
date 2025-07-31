cead361fdba8e6ec295a5b880b6280dc82c42830
fylart <fylartthrowaway0@gmail.com>
2025-08-01 05:49:16 +0800

    profile dashboard design prototype - done

--- Game/LetterTracing/scenes/letters/LetterCards/letterCard.gd
+++ Game/LetterTracing/scenes/letters/LetterCards/letterCard.gd
@@ -4,3 +4,12 @@ extends Panel
 
 func showGameOver():
 	game_over_component.show()
+	var childrens = game_over_component.get_children()
+	for child in childrens:
+		if child.is_in_group("persist"):
+			child.save()
+
+
+func _on_timer_timeout() -> void:
+	showGameOver()
+	get_tree().paused = true
