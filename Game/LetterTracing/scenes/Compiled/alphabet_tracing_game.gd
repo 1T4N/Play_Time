b306919ed1056591ee7656596909f25c81dbd588
fylart <fylartthrowaway0@gmail.com>
2025-08-01 04:03:14 +0800

    Prototype of saving needs more developing

--- Game/LetterTracing/scenes/Compiled/alphabet_tracing_game.gd
+++ Game/LetterTracing/scenes/Compiled/alphabet_tracing_game.gd
@@ -1,5 +1,5 @@
 extends Control
 func _ready() -> void:
 	#this resets the data
-	globalGameData.currentGameScore = 0
-	globalGameData.LetterTracingTime = 0
+	globalGameData.resetData()
+	globalGameData.currentGameID = 0
--- GameUI/Saving/profileDataGlobals.gd
+++ GameUI/Saving/profileDataGlobals.gd
@@ -1,5 +1,11 @@
 extends Node
 var securityKey:String = "8080"
 var profileID:int
-var saveFile
+var savePath
 var saveData
+
+
+#var save_file = FileAccess.open_encrypted_with_pass("user://profileID" + str(profileID) + ".json",FileAccess.READ,ProfileDataGlobals.securityKey)
+
+#func setSaveFilePath(profileID:int = profileID):
+	#savePath = "user://profileID" + str(profileID) + ".json"
