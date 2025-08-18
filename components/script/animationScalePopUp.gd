7e325c8172175ad94b45b0d79b94fb43c14aa153
fylart <fylartthrowaway0@gmail.com>
2025-08-18 13:51:47 +0800

    about to pull their works

--- components/script/animationScalePopUp.gd
+++ components/script/animationScalePopUp.gd
@@ -1,33 +1,41 @@
 extends Button 
-var isFinishedInitializing:bool = false
+#var isFinishedInitializing:bool = false
 
-func _ready() -> void:
-	mouse_entered.connect(_on_button_mouse_entered)
-	mouse_exited.connect(_on_button_mouse_exited)
+#func _ready() -> void:
+	#mouse_entered.connect(_on_button_mouse_entered)
+	#mouse_exited.connect(_on_button_mouse_exited)
 
-func scaleUpAnimation():
-	set_pivot_offset(get_size()/2)
-	var tween = create_tween().set_ease(Tween.EASE_OUT)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self,"scale",Vector2.ZERO,0)
-	tween.tween_property(self,"self_modulate",Color.WHITE,0.3)
-	tween.tween_property(self,"scale",Vector2.ONE,0.3)
+#
+#func scaleUpAnimation():
+	#set_pivot_offset(get_size()/2)
+	#var tween = create_tween()
+	#tween.tween_property(self,"scale",Vector2.ZERO,1)
+	#tween.tween_property(self,"position",Vector2(22,22),1)
+	#print("I'm descale")
+	#await tween.finished
+	#pop()
+	#
+#func pop():
+	#set_pivot_offset(get_size()/2)
+	#var tween = create_tween().set_ease(Tween.EASE_OUT)
+	#tween.tween_property(self,"self_modulate",Color.WHITE,0.3)
+	#tween.tween_property(self,"scale",Vector2.ONE,1)
+	#print("I'm scaling")
 
 
-
-@export var hoverAnimationScale:float = 1.1
-
-func _on_button_mouse_entered():
-	if not isFinishedInitializing:
-		return
-	set_default_cursor_shape(CURSOR_POINTING_HAND)
-	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self,"scale",Vector2(hoverAnimationScale, hoverAnimationScale), 0.3)
-	pass
-
-func _on_button_mouse_exited():
-	if not isFinishedInitializing:
-		return
-	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self,"scale",Vector2.ONE, 0.3)
-	pass
+#@export var hoverAnimationScale:float = 1.1
+#
+#func _on_button_mouse_entered():
+	#if not isFinishedInitializing:
+		#return
+	#set_default_cursor_shape(CURSOR_POINTING_HAND)
+	#var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	#tween.tween_property(self,"scale",Vector2(hoverAnimationScale, hoverAnimationScale), 0.3)
+	#pass
+#
+#func _on_button_mouse_exited():
+	#if not isFinishedInitializing:
+		#return
+	#var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	#tween.tween_property(self,"scale",Vector2.ONE, 0.3)
+	#pass
